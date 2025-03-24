a90f5678c65b893ee8dcceb7368225caa89bde24
Randula Tharaka <iamrandula@gmail.com>
2025-03-24 20:19:52 +0530

    Create Header Compnt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./components/Header.jsx";
+import { Container } from "react-bootstrap";
 
 const App = () => {
-  return <h1>Welcome to TechMartX</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to TechMartX</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;

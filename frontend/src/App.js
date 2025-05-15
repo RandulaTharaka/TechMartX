7a3c28221ca3932a6b94421847332853c61bde0d
Randula Tharaka <iamrandula@gmail.com>
2025-05-15 18:56:00 +0530

    Product List v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
 import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <SearchBox />
+              {/* <SearchBox /> */}
 
               <Nav.Link href="/cart">
                 <FaShoppingCart />

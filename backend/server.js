af8101ac390399a9a2e9d51524cbb5a6158337d8
Randula Tharaka <iamrandula@gmail.com>
2025-08-02 15:43:59 +0530

    Finished Project V6 installed dotenv V2

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import path from "path";
 import express from "express"; // "type: module"
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; // for parsing cookies in requests
-dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";

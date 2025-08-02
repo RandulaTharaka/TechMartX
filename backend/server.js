40c9d49e7b98e641a8d6cddb3456631a773e6581
Randula Tharaka <iamrandula@gmail.com>
2025-08-02 15:41:07 +0530

    Finished Project V6 installed dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import path from "path";
 import express from "express"; // "type: module"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; // for parsing cookies in requests
+dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -54,7 +56,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  import dotenv from "dotenv";
   app.get(`/`, (req, res) => {
     res.send(`API is running...`);
   });

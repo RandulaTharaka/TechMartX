5eeb997f84c03a5bbba672f7331763fd6f82ad41
Randula Tharaka <iamrandula@gmail.com>
2025-08-04 10:23:51 +0530

    Project Finished V24: WorkingTestOrderPay

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,14 +1,50 @@
 import Order from "../models/orderModel.js";
 import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+// secure paypal payment
+/* import Product from "../models/productModel.js";
 import { calcPrices } from "../utils/calcPrices.js";
 import { verifyPayPalPayment, checkIfNewTransaction } from "../utils/paypal.js";
-
+ */
 // @desc Create new order
 // @route POST /api/orders
 // @access Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
+});
+
+// secure paypal payment
+/* const addOrderItems = asyncHandler(async (req, res) => {
   const { orderItems, shippingAddress, paymentMethod } = req.body;
 
   if (orderItems && orderItems.length === 0) {
@@ -52,7 +88,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
     res.status(201).json(createdOrder);
   }
-});
+}); */
 
 // @desc Get logged in user orders
 // @route GET / api/orders/myorders
@@ -85,7 +121,31 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @desc Update order to paid
 // @route PUT / api/orders/:id/pay
 // @access Private
+
 const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// secure paypal payment
+/* const updateOrderToPaid = asyncHandler(async (req, res) => {
   const { verified, value } = await verifyPayPalPayment(req.body.id);
   if (!verified) throw new Error("Payment not verified");
 
@@ -116,7 +176,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Order not found");
   }
-});
+}); */
 
 // @desc Update to delivered
 // @route PUT / api/orders/:id/deliver
